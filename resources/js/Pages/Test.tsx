201ebf8032bd56104bd1d96ed3536a4dc7537980
Kim Rune Møller <kimrune@hotmail.no>
2024-10-22 15:44:33 +0200

    nothing special

--- resources/js/Pages/Test.tsx
+++ resources/js/Pages/Test.tsx
@@ -1,3 +1,43 @@
-export default function Test({ message }: { message: string }) {
-    return <div>{message}</div>;
+import { useState } from 'react';
+
+interface TestProps {
+    message: string;
+    data: { name: string; id: number }[];
+}
+export default function Test({ message, data }: TestProps) {
+    const [userList, setUserList] = useState(data);
+    const [newUser, setNewUser] = useState('');
+
+    function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setNewUser(e.target.value);
+    }
+
+    function handleOnClick(e: React.MouseEvent<HTMLButtonElement>) {
+        const newUserList = [
+            ...userList,
+            { name: newUser, id: userList.length + 1 },
+        ];
+        setUserList(newUserList);
+        setNewUser('');
+    }
+    return (
+        <div>
+            <h1>{message}</h1>
+            <ul>
+                {userList.map((item) => (
+                    <li key={item.id}>
+                        {item.id} - {item.name}
+                    </li>
+                ))}
+            </ul>
+            <input
+                type="text"
+                name="newUser"
+                id="newUser"
+                value={newUser}
+                onChange={handleOnChange}
+            />
+            <button onClick={handleOnClick}>Add</button>
+        </div>
+    );
 }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Route::POST('/post-test', function (Request $request) {
     return response()->json([
         'message' => 'This is a test response',
+
     ]);
 });
 
@@ -28,6 +29,14 @@ Route::get('/dashboard', function () {
 Route::get('/test', function () {
     return Inertia::render('Test', [
         'message' => 'This is a test message',
+        'data' => [
+            ['id' => 1, 'name' => 'John Doe'],
+            ['id' => 2, 'name' => 'Jane Doe'],
+            ['id' => 3, 'name' => 'John Smith'],
+            ['id' => 4, 'name' => 'Jane Smith'],
+            ['id' => 5, 'name' => 'Tommy Doe'],
+            ['id' => 6, 'name' => 'Tommy Smith'],
+        ]
     ]);
 })->middleware(['auth', 'verified'])->name('test');
 
